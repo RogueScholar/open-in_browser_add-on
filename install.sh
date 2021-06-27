3765ebed8083fc687297542ee0e90e6cf41c88fb
Peter J. Mello <RogueScholar@users.noreply.github.com>
2021-06-26 21:01:41 -0700

    Rework install script internals
    The script now breaks down into two functions, archTest and nodeSelect, which collectively test for the presence of a working NodeJS installation of sufficiently recent vintage or the ability to use the included node executable. The end result is that the script now executes without needing to change directory and the actual call to node that acts on `app/install.js` uses variables defined by the functions to call on the right interpreter with the necessary arguments without case logic and the script will error out with actionable messages before reaching it if installation is impossible.
    
    The test for $OSTYPE = darwin is removed since the release of Apple Silicon means that systems with ARM architecture can now meet the test but can't use the included x64 node.

--- install.sh
+++ install.sh
@@ -1,28 +1,55 @@
 #!/usr/bin/env bash
+set -e
 
-cd "$(dirname "$0")/app"
+typeset APP_DIR="$(dirname "${BASH_SOURCE[0]}")/app"
+typeset -g SYSNODE_BIN="$(command -v node 2>/dev/null)"
 
-which node 2>/dev/null
-isNode=$?
-echo NodeJS status = $isNode
+archTest() {
+  typeset ARCH_TYPE="$(uname -m)"
+  case ${ARCH_TYPE} in
+    x86_64)
+      typeset -g INSTALL_NODE="./node/x64/node"
+      ;;
+    i386 | i686)
+      typeset -g INSTALL_NODE="./node/x86/node"
+      ;;
+    *)
+      if [[ -z ${SYSNODE_BIN} ]]; then
+        echo "Your system doesn't have a working NodeJS."
+        echo "Please install it from https://nodejs.org/download/ and retry."
+        exit 1
+      fi
+      ;;
+  esac
+}
 
-if [ $isNode -eq 0 ]; then
-  node -e "process.exit(Number(process.version.substr(1).split('.')[0]) > 5 ? 0 : 1)"
-  isNode=$?
-fi
-if [ $isNode -eq 0 ]; then
-  echo "Installer is using your system NodeJS."
-  echo
-  node install.js `which node` $1
-else
-  MACHINE_TYPE=`uname -m`
-  echo "Installer is using the embedded NodeJS"
-  echo
-  if [[ $OSTYPE == 'darwin'* ]]; then
-    ../node/x64/node install.js --add_node $1
-  elif [ ${MACHINE_TYPE} == 'x86_64' ]; then
-    ../node/x64/node install.js --add_node $1
+nodeSelect() {
+  typeset -ga INSTALL_ARGS
+  if [[ -n ${SYSNODE_BIN} ]]; then
+    typeset SYSNODE_VER="$(node -e \
+      "console.log(process.version.substr(1).split('.')[0])" 2>/dev/null)"
+    if [[ ${SYSNODE_VER} -ge 6 ]]; then
+      echo -e "You have NodeJS v${SYSNODE_VER} installed. We'll use that!\\n"
+      typeset -g INSTALL_NODE="${SYSNODE_BIN}"
+      INSTALL_ARGS=("${SYSNODE_BIN}")
+    elif [[ -n ${INSTALL_NODE} ]]; then
+      echo -e "Your NodeJS is old, but don't worry, we brought our own! :)\\n"
+      INSTALL_ARGS=(--add_node)
+    fi
+  elif [[ -n ${INSTALL_NODE} ]]; then
+    echo -e "You don't have NodeJS but don't worry, we brought our own! :)\\n"
+    INSTALL_ARGS=(--add_node)
   else
-    ../node/x86/node install.js --add_node $1
+    echo "Your system doesn't have a working NodeJS."
+    echo "Please install it from https://nodejs.org/download/ and try again."
+    exit 1
   fi
-fi
+}
+
+archTest && nodeSelect
+echo "Proceeding with installation..."
+INSTALL_ARGS+=("${@}")
+
+"${INSTALL_NODE}" "${APP_DIR}/install.js" "${INSTALL_ARGS[@]}"
+
+echo "Installation complete. Enjoy!"
